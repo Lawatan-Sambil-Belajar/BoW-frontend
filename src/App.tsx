35f22fae9e1058b58f2afa4cae2158ca637b3b18
weihanyau <weihanyau@gmail.com>
2024-05-22 01:01:07 +0800

    fix extra import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Container } from '@mantine/core';
+
 function App() {
-    return <div />;
+    return (
+        <Container>
+            <h1>Hello, world!</h1>
+        </Container>
+    );
 }
 
 // eslint-disable-next-line import/no-default-export
